e149d7e3bcebabe7204e8d63e75c7ce1bcbbe32d
sub-cero <backupakky@gmail.com>
2026-01-29 22:22:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: "*", // Erlaubt Neocities den Zugriff
+    origin: "*", // Erlaubt deiner Neocities-Seite den Zugriff
     methods: ["GET", "POST"]
   }
 });
 
+// Was passiert, wenn jemand den Chat betritt?
 io.on('connection', (socket) => {
+  console.log('Ein Nutzer ist verbunden');
+
+  // Wenn eine Nachricht reinkommt...
   socket.on('message', (data) => {
+    // ...schicke sie sofort an ALLE verbundenen Nutzer weiter
     io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Ein Nutzer hat den Chat verlassen');
   });
 });
 
+// Der Port wird von Render automatisch zugewiesen
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log('Server läuft auf Port ' + PORT);
