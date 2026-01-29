23b92b542a8e5d37c336c8c2d45cf7acbe9df49a
sub-cero <backupakky@gmail.com>
2026-01-29 22:44:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,31 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
     origin: "*", // Erlaubt Neocities den Zugriff
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
+    credentials: true
   }
 });
 
-// NEU: Diese Zeile löst das "CAN NOT GET /"
+// Bestätigungsseite für den Browser
 app.get('/', (req, res) => {
-  res.send('<h1>Subzero Chat-Server ist ONLINE!</h1><p>Du kannst dieses Fenster jetzt schliessen und den Chat auf Neocities nutzen.</p>');
+  res.send('<h1>Subzero Chat-Server ist ONLINE</h1>');
 });
 
 io.on('connection', (socket) => {
-  console.log('Ein Nutzer ist verbunden');
+  console.log('Ein Nutzer hat sich verbunden');
+
   socket.on('message', (data) => {
-    io.emit('message', data);
+    // Schickt die Nachricht an alle (auch an dich selbst)
+    io.emit('message', {
+      name: data.name,
+      text: data.text,
+      color: data.color,
+      time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Ein Nutzer ist gegangen');
   });
 });
 
