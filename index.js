0e3cd41b62c8c80512f3a1fc6ff29d857c386374
sub-cero <backupakky@gmail.com>
2026-01-30 00:29:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Wir erlauben ALLES, damit Safari nicht meckert
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 let messages = [];
 
-app.get('/', (req, res) => {
-    res.send('SERVER_IS_RUNNING_WELL');
-});
+app.get('/', (req, res) => res.send('SERVER_IS_ALIVE'));
 
 app.get('/messages', (req, res) => {
     res.json(messages);
 });
 
-// Wir fügen eine GET-Variante zum Senden hinzu (Safari blockiert GET fast nie)
-app.get('/send_get', (req, res) => {
+// Der JSONP-Sender: Safari denkt, es lädt nur ein Script
+app.get('/send_safe', (req, res) => {
     const { user, text } = req.query;
     if (text) {
         messages.push({
@@ -27,12 +22,10 @@ app.get('/send_get', (req, res) => {
             time: new Date().toLocaleTimeString()
         });
         if (messages.length > 50) messages.shift();
-        return res.send("OK");
     }
-    res.send("FEHLER");
+    // Wir schicken gültigen JavaScript-Code zurück, damit Safari zufrieden ist
+    res.send("console.log('Message received');");
 });
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log('Server läuft auf Port ' + PORT);
-});
+app.listen(PORT, '0.0.0.0', () => console.log('Safe-Server Online'));
